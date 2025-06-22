e527f99e71539000884d9509477cd0d9694e3934
lilgibs <lil.gibran01@gmail.com>
2025-06-22 11:01:07 +0700

    update: Welcome mobile optimizing 22/06/25

--- src/pages/WelcomeView.tsx
+++ src/pages/WelcomeView.tsx
@@ -2,16 +2,19 @@ import hero_image from '../assets/jpg/hero_image.jpg'
 
 export default function WelcomeView() {
   return (
-    <div className='p-4 md:p-6 max-h-[calc(100dvh-72px-32px-72px)] md:max-h-[calc(100dvh-72px-48px-72px)] flex flex-col gap-4 bg-white rounded-lg shadow'>
-      <h1 className="text-3xl font-bold text-center">Mini App 3D Factory Floor Plan</h1>
-      <p className="text-center text-gray-600 text-lg">
+    <div className='p-4 md:p-6 h-[calc(100dvh-72px-32px-72px)] md:h-[calc(100dvh-72px-48px-72px)] flex flex-col gap-4 bg-white rounded-lg shadow'>
+      <h1 className="text-2xl md:text-3xl font-bold text-center">Mini App 3D Factory Floor Plan</h1>
+      <p className="text-center text-gray-600 text-base md:text-lg">
         An interactive 3D dashboard that visualizes a factory floor layout and displays real-time machine performance data through clickable tags.
       </p>
-      <img
-        className='h-full object-contain rounded-md'
-        src={hero_image}
-        alt='hero image'
-      />
+      <div className='flex-1 flex items-center justify-center overflow-hidden'>
+        <img
+          src={hero_image}
+          className='h-full max-w-full object-contain'
+          alt='Factory floor plan'
+        />
+      </div>
     </div>
+
   )
 }
